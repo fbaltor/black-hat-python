e8df16f20173c374890004ae5654fa11ab4e8221
Felipe Baltor <fbaltor@gmail.com>
2022-06-12 01:25:32 -0300

    feat: tcp server working

--- tcp_client.py
+++ tcp_client.py
@@ -1,13 +1,13 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "0.0.0.0"
+target_port = 9998 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"Hello, man")
 
 response = client.recv(4096)
 
